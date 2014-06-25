7a0f9e5e17410065571211764ad23c9eae939602
sloyola <santiago.loyola>
2014-06-24 23:34:12 -0500

    datos encuestas

---
+++ app/database/seeds/TipoEncuestasTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+class TipoEncuestasTableSeeder extends Seeder {
+public function run()
+    {
+        // !!! All existing users are deleted !!!
+        DB::table('tipo_encuestas')->delete();
+
+        TipoEncuestas::create(array(
+            'nombre'  => 'CONTROL DE HIGIENE DEL PERSONAL DE BARES Y COMEDORES DE LA PUCE'
+        ));
+
+        TipoEncuestas::create(array(
+            'nombre'  => 'CONTROL DE MANIPULACIÓN DE ALIMENTOS E HIGIENE DE LOS COMEDORES DE LA PUCE'
+        ));
+
+        TipoEncuestas::create(array(
+            'nombre'  => 'CONTROL DE MANIPULACIÓN DE ALIMENTOS E HIGIENE DE LOS BARES DE LA PUCE'
+        ));
+
+    }
+}
---
+++ app/models/EncabezadoPreguntasEncuestas.php
@@ -0,0 +1,9 @@
+<?php
+
+class EncabezadoPreguntasEncuestas extends Eloquent {
+	protected $guarded = array('id');
+	protected $table = 'encabezado_preguntas_encuestas';
+	public static $rules = array();
+	
+	
+}
