ce31e2ad8d181a83b99f736d5ec6f5ccee25ad02
sloyola <santiago.loyola>
2014-06-29 22:06:25 -0500

    delete app/models/EncabezadoPreguntasEncuestas.php

--- app/models/EncabezadoPreguntasEncuestas.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class EncabezadoPreguntasEncuestas extends Eloquent {
-	protected $guarded = array('id');
-	protected $table = 'encabezado_preguntas_encuestas';
-	public static $rules = array();
-	
-	
-}
