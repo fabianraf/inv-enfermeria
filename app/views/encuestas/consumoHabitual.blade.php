178b1d6ca6ab6ba258686ccd8e7ea1750b5e01d7
root <root@localhost.localdomain>
2014-07-03 08:47:24 -0700

    autocomplete

--- app/views/encuestas/consumoHabitual.blade.php
+++ app/views/encuestas/consumoHabitual.blade.php
@@ -3,6 +3,31 @@
 @section('content')
 
 {{ Form::open(array('url' => 'encuesta_consumo_alimentos')) }}
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.0/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
+<link rel="stylesheet" href="/resources/demos/style.css">
+<script>
+$(function() {
+var availableTags = [
+"Arroz",
+"Jamón",
+"Queso cheddar",
+"Pan de dulce",
+"Pan de sal",
+"Pan integral",
+"Leche entera",
+"Leche descremada",
+"Queso mozarella",
+"Piña",
+"Sandía"
+];
+$( "#tags" ).autocomplete({
+source: availableTags
+});
+});
+</script>
+
 <div class="col-lg-12">
 	<h2>Consumo habitual de alimentos
 		<div class="pull-right">
@@ -51,6 +76,7 @@
 		      Desayuno
 		    </td>
 		    <td>{{Form::text('horario', Input::old('horario'), array('class'=>'form-control'))}}</td>
+
 		    <td>
 				<select>
 					<option>--Seleccione--</option>
@@ -68,7 +94,8 @@
 					
 				</select>
 		    </td>
-		    <td>{{Form::text('ingrediente', Input::old('ingrediente'), array('class'=>'form-control'))}}</td>
+		    <!--<td>{{Form::text('ingrediente', Input::old('ingrediente'), array('class'=>'form-control'))}}</td>-->
+		    <td><input id="tags"></td>
 		    <td>
 				<select>
 					<option>--Seleccione--</option>
@@ -106,7 +133,7 @@
 					
 				</select>
 		    </td>
-		    <td>{{Form::text('ingrediente', Input::old('ingrediente'), array('class'=>'form-control'))}}</td>		    
+		    <td>{{Form::text('ingrediente', Input::old('ingrediente'), array('class'=>'form-control','id'=>'ingrediente'))}}</td>		    
 		    <td>
 				<select>
 					<option>--Seleccione--</option>
