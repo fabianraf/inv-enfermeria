3fc1b2c4a797b6b198ca0adaa4dd5395d10d4950
fabianraf <fabianraf@gmail.com>
2014-10-20 16:39:54 -0500

    Fixed styles

--- app/views/encuestas/controlHigienePersonal.blade.php
+++ app/views/encuestas/controlHigienePersonal.blade.php
@@ -13,19 +13,23 @@
 		</div>
 	</h2>
 	<hr>
-	<div class="form-group">
+
+
+
+	<div class="row top30 form-group">
 		{{ Form::label('nombreEmpresa', 'Nombre de la Empresa',array('class' => 'col-sm-2 control-label')); }}
 		<div class="col-sm-6">
 			{{ Form::text('nombreEmpresa','', array('class' => 'form-control', 'placeholder' => 'Nombre de la empresa' )); }}
 		</div>
-	</div><br><br>
-	<div class="form-group">
+	</div>
+
+	<div class="row top10 form-group">
 		{{ Form::label('nombrePropietario', 'Nombre del Propietario',array('class' => 'col-sm-2 control-label')); }}
 		<div class="col-sm-6">
 			{{ Form::text('nombrePropietario','', array('class' => 'form-control', 'placeholder' => 'Nombre del propietario' )); }}
 		</div>
-	</div><br>
-	<div class="form-group">
+	</div>
+	<div class="row top10 form-group">
 		{{ Form::label('fechaEncuesta', 'Fecha',array('class' => 'col-sm-2 control-label')); }}
 		<div class="col-sm-6">
 			{{ Form::text('fechaEncuesta', '', array('class' => 'form-control','placeholder' => 'Fecha','id' => 'datepicker','data-date-format'=>'yyyy-mm-dd','readonly')) }}
@@ -35,8 +39,8 @@
 				});
 			</script>
 		</div>
-	</div><br>	
-	<div class="form-group">
+	</div>
+	<div class="row top10 form-group">
 		{{ Form::label('observaciones', 'Observaciones',array('class' => 'col-sm-2 control-label')); }}
 		<div class="col-sm-6">
 			{{ Form::textarea('observaciones','', array('class' => 'form-control', 'placeholder' => 'Observaciones' )); }}
