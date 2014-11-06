1e3d43b34841b077f8f2a58eaa0d077f0e598c45
Fabian Aguirre <fabianraf@gmail.com>
2014-11-06 01:42:58 -0500

    Archivos no necesitados

--- app/models/EncuestasManipulacionComedores.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class EncuestasManipulacionComedores extends Eloquent {
-	protected $guarded = array();
-	protected $table = 'encuestas_manipulacion_comedores';
-	public static $rules = array();
-}
