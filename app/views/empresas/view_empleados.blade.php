f062f7c34565433ba1e44726016fa125abd9b437
root <root@localhost.localdomain>
2014-11-10 13:06:52 -0800

    anadir nuevos empleados

--- app/views/empresas/view_empleados.blade.php
+++ app/views/empresas/view_empleados.blade.php
@@ -5,7 +5,7 @@
 <div class="col-lg-12">
   <div class="pull-right">
 			<a href="/encuesta_control_higiene_personal/empresas"><input type="button" value="VOLVER A EMPRESAS" class="btn btn-warning"></a>
-			<input type="button" value="AÑADIR NUEVO EMPLEADO" class="btn btn-primary" id="anadir-nuevo-empleado">			
+			<?php  echo "<td><a href='/encuesta_control_higiene_personal/nueva_encuesta?empresa_id=".$empresa->id."'>"; ?><input type="button" value="AÑADIR NUEVO EMPLEADO" class="btn btn-primary"></a>
 	</div>
   <h2>Empleados de la empresa {{ $empresa->nombre}}</h2>
   <br><br>
