bfd3fc307dbab8b1d98066ddefb0886439ad30f4
root <root@localhost.localdomain>
2014-12-12 12:22:34 -0800

    detalle consumo habitual

--- app/views/reportes/consumo_habitual_detalle.blade.php
+++ app/views/reportes/consumo_habitual_detalle.blade.php
@@ -362,6 +362,86 @@
             </table>                        
         </div>      
     </div>
+    <?php 
+    $totalCHO = 0;
+    $totalPROT = 0;
+    $totalGRAS = 0;
+    $totalKcal = 0;
+    ?>
+    @foreach($estudiante->consumoHabitualDeAlimento as $consumoHabitual)
+        @foreach($consumoHabitual->preparacionConsumoHabitualDeAlimentos as $preparacionCHA)
+            @foreach($preparacionCHA->ingredientesPreparacionConsumoHabitualDeAlimento as $ingrediente)
+                <?php 
+                    if($ingrediente->grupo_de_alimentos=='L'){
+                        $totalCHO += $ingrediente->numero_de_porciones*0;
+                        $totalPROT += $ingrediente->numero_de_porciones*0;
+                        $totalGRAS += $ingrediente->numero_de_porciones*0;
+                        $totalKcal += $ingrediente->numero_de_porciones*0;
+                    }
+                    if($ingrediente->grupo_de_alimentos=='LE'){
+                        $totalCHO += $ingrediente->numero_de_porciones*12;
+                        $totalPROT += $ingrediente->numero_de_porciones*8;
+                        $totalGRAS += $ingrediente->numero_de_porciones*8;
+                        $totalKcal += $ingrediente->numero_de_porciones*150;
+                    }
+                    if($ingrediente->grupo_de_alimentos=='LSD'){
+                        $totalCHO += $ingrediente->numero_de_porciones*12;
+                        $totalPROT += $ingrediente->numero_de_porciones*8;
+                        $totalGRAS += $ingrediente->numero_de_porciones*5;
+                        $totalKcal += $ingrediente->numero_de_porciones*120;
+                    }
+                    if($ingrediente->grupo_de_alimentos=='LD'){
+                        $totalCHO += $ingrediente->numero_de_porciones*12;
+                        $totalPROT += $ingrediente->numero_de_porciones*8;
+                        $totalGRAS += $ingrediente->numero_de_porciones*1;
+                        $totalKcal += $ingrediente->numero_de_porciones*90;
+                    }
+                    if($ingrediente->grupo_de_alimentos=='V'){
+                        $totalCHO += $ingrediente->numero_de_porciones*5;
+                        $totalPROT += $ingrediente->numero_de_porciones*2;
+                        $totalGRAS += $ingrediente->numero_de_porciones*0;
+                        $totalKcal += $ingrediente->numero_de_porciones*25;
+                    }
+                    if($ingrediente->grupo_de_alimentos=='F'){
+                        $totalCHO += $ingrediente->numero_de_porciones*15;
+                        $totalPROT += $ingrediente->numero_de_porciones*0;
+                        $totalGRAS += $ingrediente->numero_de_porciones*0;
+                        $totalKcal += $ingrediente->numero_de_porciones*60;
+                    }
+                    if($ingrediente->grupo_de_alimentos=='Al'){
+                        $totalCHO += $ingrediente->numero_de_porciones*15;
+                        $totalPROT += $ingrediente->numero_de_porciones*2;
+                        $totalGRAS += $ingrediente->numero_de_porciones*5;
+                        $totalKcal += $ingrediente->numero_de_porciones*115;
+                    }
+                    if($ingrediente->grupo_de_alimentos=='Az'){
+                        $totalCHO += $ingrediente->numero_de_porciones*10;
+                        $totalPROT += $ingrediente->numero_de_porciones*0;
+                        $totalGRAS += $ingrediente->numero_de_porciones*0;
+                        $totalKcal += $ingrediente->numero_de_porciones*40;
+                    }
+                    if($ingrediente->grupo_de_alimentos=='C'){
+                        $totalCHO += $ingrediente->numero_de_porciones*0;
+                        $totalPROT += $ingrediente->numero_de_porciones*7;
+                        $totalGRAS += $ingrediente->numero_de_porciones*5;
+                        $totalKcal += $ingrediente->numero_de_porciones*75;
+                    }
+                    if($ingrediente->grupo_de_alimentos=='G'){
+                        $totalCHO += $ingrediente->numero_de_porciones*0;
+                        $totalPROT += $ingrediente->numero_de_porciones*0;
+                        $totalGRAS += $ingrediente->numero_de_porciones*5;
+                        $totalKcal += $ingrediente->numero_de_porciones*45;
+                    }                                    
+                ?>
+            @endforeach
+        @endforeach
+    @endforeach
+    <div class="col-sm-6">
+        <p><li><strong>CHO (g): </strong>{{ round($totalCHO,2) }}</p>
+        <p><li><strong>PROT (g): </strong>{{ round($totalPROT,2) }}</p>
+        <p><li><strong>GRAS (g): </strong>{{ round($totalGRAS,2) }}</p>
+        <p><li><strong>Kcal Total: </strong>{{ round($totalKcal,2) }}</p>
+    </div> 
 </div>
 
 @endif
